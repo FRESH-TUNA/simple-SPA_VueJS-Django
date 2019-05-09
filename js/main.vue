7f38702c867c56eefdcfe95649c1003505e1d357
DONGWON KIM <lunacircle4@gmail.com>
2019-05-09 12:45:25 +0900

    initial commit

---
+++ js/main.vue
@@ -0,0 +1,102 @@
+Vue.component('boardhead',{
+    template:
+    `
+    <tr>
+        <th>번호</th>
+        <th>날짜</th>
+        <th>제목</th>
+        <th colspan="2"></th>
+    </tr>
+    `
+})
+
+Vue.component('boardrow',{
+    props: ['element'],
+    template:
+    `
+    <tr>
+        <td>{{element.id}}</td>
+        <td>{{element.date}}</td>
+        <td>{{element.title}}</td>
+        <td> 
+            <button class="btn btn-primary">update</button>
+        </td>
+        <td> 
+            <button class="btn btn-primary">delete</button>
+        </td>
+    </tr>
+    `
+})
+
+new Vue({
+    el: '#boardTable',
+    data: {
+        elements: [
+            {
+                id:1,
+                date:2,
+                title:'hoho'
+            },
+            {
+                id:2,
+                date:3,
+                title:'hihi'
+            },
+              {
+                id:2,
+                date:3,
+                title:'hihi'
+            },
+              {
+                id:2,
+                date:3,
+                title:'hihi'
+            },
+              {
+                id:2,
+                date:3,
+                title:'hihi'
+            },
+              {
+                id:2,
+                date:3,
+                title:'hihi'
+            },
+              {
+                id:2,
+                date:3,
+                title:'hihi'
+            },
+              {
+                id:2,
+                date:3,
+                title:'hihi'
+            },
+            {
+                id:2,
+                date:3,
+                title:'hihi'
+            },
+              {
+                id:2,
+                date:3,
+                title:'hihi'
+            },
+              {
+                id:2,
+                date:3,
+                title:'hihi'
+            },
+              {
+                id:2,
+                date:3,
+                title:'hihi'
+            },
+            {
+                id:2,
+                date:3,
+                title:'hihi'
+            },
+        ]
+    }
+});
