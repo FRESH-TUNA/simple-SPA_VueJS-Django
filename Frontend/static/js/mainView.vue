4450bbb7e30396ae0790e1cc5e98ec12bacc8728
DONGWON KIM <lunacircle4@gmail.com>
2019-05-12 19:04:44 +0900

    VueJS 간단한 화면 비동기 변경 구현

---
+++ Frontend/static/js/mainView.vue
@@ -0,0 +1,101 @@
+var postTemplate = {
+  template:
+  `
+    <div>
+      야옹
+    </div>
+  `
+}
+
+var detailTemplate = {
+  template:
+  `
+    <div>
+      멍멍
+    </div>
+  `
+}
+
+var mainTemplate = {
+  /*
+  props: {
+    elements: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  },*/
+  data: function() {
+    return {
+      elements: [
+        {
+            id:1,
+            date:2,
+            title:'hoho'
+        },
+        {
+            id:2,
+            date:3,
+            title:'hihi'
+        },
+        {
+            id:2,
+            date:3,
+            title:'hihi'
+        },
+        {
+            id:2,
+            date:3,
+            title:'hihi'
+        }
+      ]
+    }
+  },
+  template:
+  `
+    <div>
+      <div v-for="element in elements" :element="element" :key="element.id">
+        <a>{{element.id}}</a>
+        <a>{{element.date}}</a>
+        <a>{{element.title}}</a>
+        <button class="btn btn-primary">update</button>
+        <button class="btn btn-primary">delete</button>
+      </div>
+    </div>
+  `
+}
+
+new Vue({
+    el: '#mainView',
+    data: {
+        elements: [
+            {
+                id:1,
+                date:2,
+                title:'hoho'
+            },
+            {
+                id:2,
+                date:3,
+                title:'hihi'
+            },
+            {
+                id:2,
+                date:3,
+                title:'hihi'
+            },
+            {
+                id:2,
+                date:3,
+                title:'hihi'
+            }
+        ],
+        currentView: 'main-template'
+    },
+    components: {
+      'main-template':mainTemplate,
+      'post-template':postTemplate,
+      'detail-template':detailTemplate
+    }
+  });
---
+++ Frontend/static/js/nav.vue
@@ -0,0 +1,22 @@
+
+
+var navTemplate = {
+    template: 
+    `
+    <nav class="navbar navbar-expand-lg navbar-light">
+            <a class="navbar-brand" href="">{{title}}</a>
+    </nav>
+    `,
+    data: function(){
+        return {
+            title: 'TunaBoard'
+        }  
+    }
+}
+
+var nav = new Vue({
+    el: '#nav',
+    components: {
+        'nav-template': navTemplate
+    }
+})
