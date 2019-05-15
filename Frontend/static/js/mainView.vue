4653828c6673c5076cbdb8c74d61a173e2f62826
DONGWON KIM <lunacircle4@gmail.com>
2019-05-15 23:42:37 +0900

    read posts 구현

--- Frontend/static/js/mainView.vue
+++ Frontend/static/js/mainView.vue
@@ -7,64 +7,62 @@ var postTemplate = {
   `
 }
 
-var detailTemplate = {
-  template:
-  `
-    <div>
-      멍멍
-    </div>
-  `
-}
-
-var mainTemplate = {
+var createTemplate = {
   data: function() {
     return {
-      elements: [
-        {
-            id:1,
-            date:2,
-            title:'hoho',
-            content: '안녕하세요'
-        },
-        {
-            id:2,
-            date:3,
-            title:'hihi',
-            content: '안녕하세요'
-        },
-        {
-            id:2,
-            date:3,
-            title:'hihi',
-            content: '안녕하세요'
-        },
-        {
-            id:2,
-            date:3,
-            title:'hihi',
-            content: '안녕하세요'
-        }
-      ],
-      postsStyleObject: {
-        padding: '5%',
-      },
-      postStyleObject: {
-        width: '100%',
-        border: '1px solid black',
-        'margin-top': '10px'
+      textareaStyleObject: {
+        height: '300px'
       }
     }
   },
   template:
   `
-    <div v-bind:style="postsStyleObject">
-      <div v-for="element in elements" :element="element" :key="element.id" v-bind:style="postStyleObject">
-        <h3>{{element.title}}</h3>
+    <form>
+      <div class="form-group">
+        <label for="title">Title</label>
+        <input type="text" class="form-control" placeholder="Enter title">
+      </div>
+      <div class="form-group">
+        <label for="content">Content</label>
+        <textarea class="form-control" v-bind:style="textareaStyleObject"></textarea>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  `
+}
+
+var mainTemplate = {
+  props: ['currentViewData'],
+  data: function() {
+    return {
+      postStyleObject: {
+        width: '100%',
+        border: '1px solid black',
+        'margin-top': '10px'
+      },
+      postTitleStyleObject: {
+        display: 'flex',
+        'justify-content': 'space-between'
+      },
+      postButtonsStyleObject: {
+        display: 'flex',
+        'flex-direction': 'row-reverse'
+      }
+    }
+  },
+  template:
+  `
+    <div>
+      <div v-for="element in currentViewData" :element="element" :key="element.id" v-bind:style="postStyleObject">
+        <div v-bind:style="postTitleStyleObject">
+          <h3>{{element.title}}</h3>
+          <a>{{element.published_date}}</a>
+        </div>
         <div>{{element.content}}</div>
-        <a>{{element.date}}</a>
-        
-        <button class="btn btn-primary">update</button>
-        <button class="btn btn-primary">delete</button>
+        <div v-bind:style="postButtonsStyleObject">
+          <button class="btn btn-primary">update</button>
+          <button class="btn btn-primary">delete</button>
+        </div>
       </div>
     </div>
   `
@@ -73,33 +71,25 @@ var mainTemplate = {
 var mainview = new Vue({
     el: '#mainView',
     data: {
-        elements: [
-            {
-                id:1,
-                date:2,
-                title:'hoho'
-            },
-            {
-                id:2,
-                date:3,
-                title:'hihi'
-            },
-            {
-                id:2,
-                date:3,
-                title:'hihi'
-            },
-            {
-                id:2,
-                date:3,
-                title:'hihi'
-            }
-        ],
+        currentViewData: [],
+        postsStyleObject: {
+          padding: '5%',
+        },
         currentView: 'main-template'
     },
     components: {
       'main-template':mainTemplate,
-      'post-template':postTemplate,
-      'detail-template':detailTemplate
+      'create-template':createTemplate,
+    },
+    methods: {
+      readPosts: function() {
+        axios.get('http://127.0.0.1:8000/api/')
+          .then(function(response){
+            mainview.currentViewData = response.data.posts; // 객체 형태로 반환. 파싱작업 불필
+          });
+      }
+    },
+    created() {
+      this.readPosts()
     }
 });
--- Frontend/static/js/nav.vue
+++ Frontend/static/js/nav.vue
@@ -4,8 +4,8 @@ var navTemplate = {
     template: 
     `
     <nav class="navbar navbar-expand-lg navbar-light">
-            <a class="navbar-brand" href="">{{title}}</a>
-                    <button type="button" class="btn btn-primary" v-on:click="mainview.currentView = 'main-template'">New Document</button>
+            <a class="navbar-brand" v-on:click="mainview.currentView = 'main-template'">{{title}}</a>
+            <button type="button" class="btn btn-primary" v-on:click="mainview.currentView = 'create-template'">New Document</button>
     </nav>
     `,
     data: function(){
